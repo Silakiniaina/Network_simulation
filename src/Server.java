a9a464ac2d3d38b4e881a2fe36735c97e76292ef
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-05-06 14:15:20 +0300

    Getters, Setters + Constructors for Server

--- src/Server.java
+++ src/Server.java
@@ -3,6 +3,36 @@ package graphe;
 import javax.swing.Server;
 
 public class Server extends JPanel{
-    String ip_adress;
+    String ipAdress;
     Vector<String> listSites;
+
+    //Constructor
+    public Server(){ 
+
+    }
+    public Server(String ip, Vector<String> sites){
+        this.setIpAdress(ip);
+        this.setListSites(sites);
+    }
+
+    // Getters
+    public String getIpAdress(){
+        return this.ipAdress;
+    } 
+    public Vector<String> getListSites(){
+        return this.listSites;
+    }
+
+    // Setters
+    public void setIpAdress(String newIp){
+        this.ipAdress = newIp;
+    }
+    public void setListSites(Vector<String> list){
+        this.listSites = list;
+    }
+
+    // Add new sites
+    public void addSites(String siteName){
+        this.getListSites().add(siteName);
+    }
 }
