b24cf523a7796f514b5dcd7f126a03decf21d8da
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-05-06 14:17:47 +0300

    Adding attribute Point in Server

--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,10 @@
 package graphe; 
 
 import javax.swing.Server;
+import java.awt.Point;
 
 public class Server extends JPanel{
+    Point position;
     String ipAdress;
     Vector<String> listSites;
 
@@ -22,6 +24,9 @@ public class Server extends JPanel{
     public Vector<String> getListSites(){
         return this.listSites;
     }
+    public Point getPosition(){
+        return this.position;
+    }
 
     // Setters
     public void setIpAdress(String newIp){
@@ -29,6 +34,9 @@ public class Server extends JPanel{
     }
     public void setListSites(Vector<String> list){
         this.listSites = list;
+    }
+    public void setPosition(Point p){
+        this.position = p;
     }
 
     // Add new sites
