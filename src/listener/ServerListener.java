ecb0e0b6a9faa658e1e1171a7a1887f3b32dcb67
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-05-07 20:30:46 +0300

    Initialialize ServerListener Class

---
+++ src/listener/ServerListener.java
@@ -0,0 +1,12 @@
+package listener;
+
+import java.awt.event.*;
+
+import area.Area;
+import graphe.Server;
+
+public class ServerListener implements MouseListener{
+    Server server;
+    Area area;
+    
+}
