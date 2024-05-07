5b274ebd2e4102353c0eb4200e87d66602e799af
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-05-07 20:58:27 +0300

    Creation getters, setters, and constructors for ServerMenu

--- src/form/ServerMenu.java
+++ src/form/ServerMenu.java
@@ -1,9 +1,39 @@
 package form;
 
 import javax.swing.JButton;
+import javax.swing.JPanel;
 
-public class ServerMenu{
+import graphe.Server;
+
+public class ServerMenu extends JPanel{
     JButton buttonLink;
     JButton buttonDelete;
+    Server server;
     
+    /* Constructors */
+    public ServerMenu(Server s){
+        this.setServer(s);
+    }
+    
+    /* Getters */
+    public JButton getButtonLink() {
+        return buttonLink;
+    }
+    public JButton getButtonDelete() {
+        return buttonDelete;
+    }
+    public Server getServer(){
+        return this.server;
+    }
+    
+    /* Setters */
+    public void setButtonLink(JButton buttonLink) {
+        this.buttonLink = buttonLink;
+    }
+    public void setButtonDelete(JButton buttonDelete) {
+        this.buttonDelete = buttonDelete;
+    }
+    public void setServer(Server s){
+        this.server = s;
+    }
 }
