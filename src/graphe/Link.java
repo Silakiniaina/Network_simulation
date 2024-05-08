f459f53292e582b4b00846a3630e9f3e98eaa19c
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-05-08 16:21:44 +0300

    Getters, Setters and Constructor for Link

--- src/graphe/Link.java
+++ src/graphe/Link.java
@@ -6,4 +6,33 @@ public class Link extends JPanel{
     Server source; 
     Server target;
     int ping;
+
+    /* Constructors */
+    public Link(Server source, Server target, int ping){
+        this.setSource(source);
+        this.setTarget(target);
+        this.setPing(ping);
+    }
+    
+    /* Getters */
+    public Server getSource() {
+        return source;
+    }
+    public Server getTarget() {
+        return target;
+    }
+    public int getPing() {
+        return ping;
+    }
+
+    /* Setters */
+    public void setSource(Server source) {
+        this.source = source;
+    }
+    public void setTarget(Server target) {
+        this.target = target;
+    }
+    public void setPing(int ping) {
+        this.ping = ping;
+    }
 }
