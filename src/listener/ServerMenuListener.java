5ebb2edb7c0e08776fb3dfd035e21e91c8a7f2d3
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-05-08 15:41:51 +0300

    Getters, Setters, Constructors for ServerMenuListener

--- src/listener/ServerMenuListener.java
+++ src/listener/ServerMenuListener.java
@@ -6,5 +6,19 @@ import form.ServerMenu;
 
 public class ServerMenuListener implements ActionListener{
     ServerMenu serverMenu;
+
     
+    /* Constructors */
+    public ServerMenuListener(ServerMenu s){
+        this.setServerMenu(s);
+    }
+    /* Getters */
+    public ServerMenu getServerMenu() {
+        return serverMenu;
+    }
+
+    /* Setters */
+    public void setServerMenu(ServerMenu serverMenu) {
+        this.serverMenu = serverMenu;
+    }
 }
